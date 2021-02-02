2ce511c78be1fedbaa80aaa8e3e4e483b1331de4
yangfan1374 <yf1374@hanweb.com>
2021-02-02 10:12:05 +0800

    commit all

---
+++ src/main/java/com/example/learn_nacos/LearnNacosApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn_nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnNacosApplication.class, args);
+    }
+
+}
